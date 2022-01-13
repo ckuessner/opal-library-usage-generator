0143b71e330a9cfabd8ab544d701b60d645b6733
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2022-01-13 20:38:19 +0100

    Optimize code generation in regard to use of locals

--- src/main/scala/de/ckuessner/opal/usagegen/generators/PublicClassUsageGenerator.scala
+++ src/main/scala/de/ckuessner/opal/usagegen/generators/PublicClassUsageGenerator.scala
@@ -19,35 +19,55 @@ object PublicClassUsageGenerator {
 
     val methodBody = ListBuffer.empty[LabeledInstruction]
 
-    // Instructions to initialize parameters
-    calledMethod.descriptor.parameterTypes.foreachWithIndex { case (paramType, index) =>
-      // Construct default value
-      methodBody ++= defaultValueForFieldType(paramType)
-      // Store default value
-      methodBody += storeInstruction(paramType, index)
-    }
+    // Reference types need to be stored as locals, as they are passed to sink after consumption by method call.
+    // This means that we construct the default values for reference types first and store them as locals.
+    calledMethod.descriptor.parameterTypes
+      .filter(parameter => parameter.isReferenceType)
+      .foreachWithIndex { case (paramType, localVarIndex) =>
+        // Construct default value
+        methodBody ++= defaultValueForFieldType(paramType)
+        // Store constructed value
+        methodBody += storeInstruction(paramType, localVarIndex)
+      }
 
-    // Load all parameters before call
-    calledMethod.descriptor.parameterTypes.foreachWithIndex { case (paramType, index) =>
-      methodBody += loadInstruction(paramType, index)
-    }
-
-    // Call tested API method
-    methodBody += INVOKESTATIC(classFileOfCalledMethod.thisType, isInterface = false, calledMethod.name, calledMethod.descriptor)
-
-    // Load parameters of called method. Pass-by-value parameters are not loaded (e.g., int, double, etc.), because they
-    // aren't changed by called method. The return value (if it exists is also passed to the sink method)
-    calledMethod.parameterTypes.foreachWithIndex { case (paramType, index) =>
-      paramType match {
-        case _: ReferenceType => methodBody += ALOAD(index)
-        case _: BaseType => // Ignored, because parameter is passed-by-value
+    // Load/construct parameters for method call
+    {
+      var localVarIndex = 0 // Stores the next index for a reference type parameter
+      for (paramType <- calledMethod.descriptor.parameterTypes) {
+        paramType match {
+          case _: ReferenceType =>
+            // Load parameter (since it's a reference type and was already constructed in the prior step)
+            methodBody += ALOAD(localVarIndex)
+            localVarIndex += 1
+          case _: BaseType =>
+            // Place default value on stack (base types aren't constructed in prior step)
+            methodBody ++= defaultValueForFieldType(paramType)
+        }
       }
     }
+
+    // Call method of tested library
+    methodBody += INVOKESTATIC(
+      declaringClass = classFileOfCalledMethod.thisType,
+      isInterface = false,
+      name = calledMethod.name,
+      methodDescriptor = calledMethod.descriptor
+    )
+
+    // The return value (if non-void) is on the stack after the method call
+
+    // After execution of the above method call, the reference type parameters need to be passed to sink -> load them.
+    calledMethod.descriptor.parameterTypes
+      .filter(parameter => parameter.isReferenceType)
+      .foreachWithIndex { case (_, localVarIndex) =>
+        methodBody += ALOAD(localVarIndex)
+      }
 
     // Call sink, passing return value of called method and all pass-by-reference parameters
     val sinkMethodSignature = generateSinkMethodSignature(calledMethod.returnType, calledMethod.parameterTypes)
     methodBody += INVOKESTATIC(sinkClassName, isInterface = false, sinkMethodName, sinkMethodSignature)
 
+    // Simply return, the stack should now be empty
     methodBody += RETURN
     methodBody
   }
