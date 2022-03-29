4bb72e5060a6767a6e2fe3dd47af021a346536b8
Christian Kuessner <17302779+ckuessner@users.noreply.github.com>
2022-03-29 10:11:15 +0200

    Include subtypes in InstanceSearcher

--- src/main/scala/de/ckuessner/opal/usagegen/analyses/InstanceSearcher.scala
+++ src/main/scala/de/ckuessner/opal/usagegen/analyses/InstanceSearcher.scala
@@ -2,13 +2,12 @@ package de.ckuessner.opal.usagegen.analyses
 
 import org.opalj.br.analyses.Project
 import org.opalj.br.{ClassFile, ObjectType}
+import org.opalj.collection.immutable.RefArray
 
 import java.io.File
 import java.net.URL
 
 class InstanceSearcher(private val project: Project[_]) {
-  // TODO: Include subtypes instead of only exact type matches when searching for instance sources
-
   def searchStaticNonPrivateFieldsOfType(theType: ObjectType): Iterable[StaticFieldInstanceSource] = {
     project.allFields.filter(field =>
       field.isStatic && !field.isPrivate && field.fieldType == theType
@@ -16,21 +15,26 @@ class InstanceSearcher(private val project: Project[_]) {
   }
 
   def searchNonPrivateConstructorsForType(theType: ObjectType): Iterable[ConstructorInstanceSource] = {
-    project.classFile(theType).toIterable.flatMap(_.methods.filter(
-      method => method.isConstructor && !method.isPrivate
-    )).map(ConstructorInstanceSource(theType, _))
+    project.classFile(theType).toIterable
+      .filterNot(_.isAbstract)
+      .flatMap(_.methods.filter(
+        method => method.isConstructor && !method.isPrivate
+      )).map(ConstructorInstanceSource(theType, _))
   }
 
   def searchStaticMethodsReturningType(theType: ObjectType): Iterable[StaticMethodInstanceSource] = {
     project.allMethods.filter(method =>
-      method.returnType == theType && !method.isConstructor && method.isStatic
+      !method.isPrivate && method.returnType == theType && !method.isConstructor && method.isStatic
     ).map(StaticMethodInstanceSource(theType, _))
   }
 
+  // TODO: non-static fields and methods can also provide instances, they require an instance of the containing class
+
   /**
-   * Searches all object types that are used either as a parameter of a method, or classes that have non-private instance methods in the project.
+   * The object types that are used either as a parameter of a method, or classes that have non-private instance methods
+   * in the project.
    */
-  lazy val requiredInstanceTypesInLibrary: Set[ObjectType] = {
+  lazy val requiredObjectTypesInLibrary: Set[ObjectType] = {
     // Non-private instance methods -> require instance of classFile type
     val classesWithNonPrivateInstanceMethods = project.allProjectClassFiles.toSet.filter(classFile =>
       classFile.instanceMethods.exists(!_.isPrivate)
@@ -39,21 +43,56 @@ class InstanceSearcher(private val project: Project[_]) {
     // Methods taking parameters
     val usedAsParameter = project.allProjectClassFiles.toSet.flatMap((classFile: ClassFile) =>
       classFile.methods.flatMap(method =>
-        method.descriptor.parameterTypes.filter(param => param.isObjectType) // TODO: maybe ignore ObjectType.String
+        // TODO: maybe filter out ObjectType.{Object, String} (and/or others)
+        method.descriptor.parameterTypes.filter(param => param.isObjectType)
       ).map(_.asObjectType)
     )
 
     usedAsParameter union classesWithNonPrivateInstanceMethods
   }
 
-  lazy val typeToInstanceSourcesMap: Map[ObjectType, Seq[InstanceSource]] = {
-    requiredInstanceTypesInLibrary.map(theType => {
-      val instanceSources = List.newBuilder[InstanceSource]
-      instanceSources ++= searchNonPrivateConstructorsForType(theType)
-      instanceSources ++= searchStaticNonPrivateFieldsOfType(theType)
-      instanceSources ++= searchStaticMethodsReturningType(theType)
-      theType -> instanceSources.result()
-    }).toMap
+  /**
+   * The object types that are used either as a parameter of a method, or classes that have non-private instance methods
+   * in the project. Also contains all the supertypes of these types.
+   */
+  lazy val requiredObjectTypesInLibraryWithSupertypes: Set[ObjectType] = {
+    requiredObjectTypesInLibrary.flatMap { objectType =>
+      project.classHierarchy.allSupertypes(objectType, reflexive = true)
+    }
+  }
+
+  lazy val requiredObjectTypesInLibraryWithSubtypes: Set[ObjectType] = {
+    requiredObjectTypesInLibrary.flatMap { objectType =>
+      project.classHierarchy.allSubtypes(objectType, reflexive = true)
+    }
+  }
+
+  lazy val typeToInstanceSourcesMap: Map[ObjectType, Array[InstanceSource]] = {
+    // Collect all the direct instance sources
+    val instanceSources = directInstanceSourcesForTypes(requiredObjectTypesInLibraryWithSubtypes)
+
+    // Merge instance sources to include transitive sources
+    requiredObjectTypesInLibrary.map { objectType =>
+      objectType -> transitiveSourcesForType(objectType, instanceSources)
+    }.toMap
+  }
+
+  def directInstanceSourcesForTypes(types: Set[ObjectType]): Map[ObjectType, Array[InstanceSource]] = types.map(
+    theType => theType -> directSourcesForType(theType)
+  ).toMap
+
+  def directSourcesForType(objectType: ObjectType): Array[InstanceSource] = {
+    val instanceSourcesForType = Array.newBuilder[InstanceSource]
+    instanceSourcesForType ++= searchNonPrivateConstructorsForType(objectType)
+    instanceSourcesForType ++= searchStaticNonPrivateFieldsOfType(objectType)
+    instanceSourcesForType ++= searchStaticMethodsReturningType(objectType)
+    instanceSourcesForType.result()
+  }
+
+  def transitiveSourcesForType(objectType: ObjectType, directSources: Map[ObjectType, Array[InstanceSource]]): Array[InstanceSource] = {
+    project.classHierarchy.allSubtypes(objectType, reflexive = true).flatMap { subtype: ObjectType =>
+      directSources.getOrElse(subtype, Array[InstanceSource]())
+    }.toArray
   }
 }
 
